74ca9b03ebbf52cfeb13837a52b1db171c9f9ee1
Ddd <752042257@qq.com>
2018-10-15 17:30:58 +0800

    try to delete some cases

--- main.py
+++ main.py
@@ -4,6 +4,8 @@ import xlrd
 import os
 import openpyxl
 import xlwt
+import re
+import chardet
 
 def fileload(path):
     workbook = xlrd.open_workbook(path)
@@ -11,9 +13,10 @@ def fileload(path):
     worksheet = workbook.sheet_by_name('Sheet 1')
     for i in range(1,worksheet.nrows):
         row = worksheet.row(i)
-        for j in range(0,worksheet.ncols):
-            print(worksheet.cell_value(i,j)," ")
-        print('\n')
+        if worksheet.cell_value(i, 5) == u'最后一次接诊' or worksheet.cell_value(i, 5) == u'首次接诊':
+            for j in range(0,worksheet.ncols):
+                print(worksheet.cell_value(i,j)," ")
+            print('\n')
 
 
 if __name__ == "__main__":
