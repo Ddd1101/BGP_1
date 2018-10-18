6c45b174bbb550deeeb9f81307ef356de470a6d8
Ddd <752042257@qq.com>
2018-10-18 19:04:13 +0800

    finish

--- main.py
+++ main.py
@@ -6,20 +6,51 @@ import openpyxl
 import xlwt
 import re
 import chardet
-
-def fileload(path):
-    workbook = xlrd.open_workbook(path)
-    sheet_in = workbook.sheet_names()
-    worksheet = workbook.sheet_by_name('Sheet 1')
-    for i in range(1,worksheet.nrows):
-        row = worksheet.row(i)
-        if worksheet.cell_value(i, 5) == u'最后一次接诊' or worksheet.cell_value(i, 5) == u'首次接诊':
-            for j in range(0,worksheet.ncols):
-                print(worksheet.cell_value(i,j)," ")
-            print('\n')
+from datetime import datetime,date
+import csv
+import xlwt
 
 
 if __name__ == "__main__":
     base_dir = os.getcwd()
     path = os.path.join(base_dir,'4.xlsx')
-    fileload(path)
+    workbook = xlrd.open_workbook(path)
+    worksheet = workbook.sheet_by_name('Sheet 1')
+    i = 1
+    item = {}
+    out_row = 0
+    book = xlwt.Workbook()
+    sheet = book.add_sheet('data')
+    while i < worksheet.nrows-4:
+        user_id = worksheet.cell_value(i, 1)
+        j = 0
+        while worksheet.cell_value(i, 1) == user_id:
+            item[j] = worksheet.row(i)
+            i += 1
+        newitem = sorted(item.items(), key=lambda d: d[1][3], reverse=False)
+        #for k in range(0,len(newitem)):
+        #    if newitem[k][1][5] == u'报告发布':
+        #        a = k
+        #    if newitem[k][1][5] == u'检验':
+        #        b = k
+        #    if newitem[k][1][5] == u'发药':
+        #        c = k
+        #    if newitem[k][1][5] == u'配药':
+        #       d = k
+        for k in newitem:
+            out_col = 0
+            if k[1][5].value == u'报告审核':
+                k[1][5].value = u'报告发布'
+            elif k[1][5].value == u'报告':
+                k[1][5].value = u'报告发布'
+            for h in k[1]:
+                if h.ctype == 3:
+                    tmp = xlrd.xldate_as_tuple(h.value, 0)
+                    value = datetime(*tmp[:6]).strftime('%Y/%m/%d %H:%M:%S')
+                    sheet.write(out_row, out_col, value)
+                else:
+                    sheet.write(out_row,out_col,h.value)
+                #print(h.value)
+                out_col+=1
+            out_row+=1
+        book.save('out.xls')
